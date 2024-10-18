8e887c2063e967d49ffb7f80600c073b451bb2a5
Joao Menes <joao@norton.me>
2024-10-18 13:54:23 -0300

    first commit

---
+++ comparacaoString.py
@@ -0,0 +1,8 @@
+#Comparando as duas String
+
+def solution(text, ending):
+    return text.endswith(ending)
+
+
+
+
---
+++ testes.py
@@ -0,0 +1,34 @@
+from comparacaoString import solution
+
+#Testes para corretos
+fixed_tests_True = (
+    ("samurai", "ai"),
+    ("ninja", "ja"),
+    ("sensei", "i"),
+    ("abc", "abc"),
+    ("abcabc", "bc"),
+    ("fails", "ails"),
+)
+
+# Testes que para falharem
+fixed_tests_False = (
+    ("sumo", "omo"),
+    ("samurai", "ra"),
+    ("abc", "abcd"),
+    ("ails", "fails"),
+    ("this", "fails"),
+    ("spam", "eggs"),
+)
+
+def test_solution():
+    # Retorna True
+    for text, ending in fixed_tests_True:
+        assert solution(text, ending) == True, f"Falhou: {text}.endswith({ending}) deveria ser True"
+    
+    # Retorna false
+    for text, ending in fixed_tests_False:
+        assert solution(text, ending) == False, f"Falhou: {text}.endswith({ending}) deveria ser False"
+
+
+test_solution()
+print("Todos os testes passaram!") #Caso  todos passem
